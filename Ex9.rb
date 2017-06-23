ff7a29f84488441ea79e18c26ccc9c016e88cd66
David Halewood <davidnhalewood@hotmail.com>
2017-06-23 17:21:27 +0100

    adding Ex7-10.rb

---
+++ Ex9.rb
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# \n add a new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# """ allows multiple line string when enter produces a new line
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
