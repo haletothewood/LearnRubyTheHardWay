8be585dd60c6f692accc6ac1c9d220d470717db2
David Halewood <davidnhalewood@hotmail.com>
2017-06-23 16:49:44 +0100

    switching units

--- Ex5.rb
+++ Ex5.rb
@@ -1,17 +1,19 @@
 name = "Zach A. Shaw"
 age = 35 # Not a lie in 2009
 height = 74 # inches
+hcm = height * 2.5 # cm
 weight = 180 # lbs
+wkg = weight * 0.45 # kg
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}"
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{hcm} cm tall."
+puts "He's #{wkg} kilograms heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+puts "If I add #{age}, #{hcm}, and #{wkg} I get #{age + hcm + wkg}."
