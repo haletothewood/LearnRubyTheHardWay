6cab16a1c564624e0534a0623c4e9e5eb25f85e4
David Halewood <davidnhalewood@hotmail.com>
2017-06-24 13:05:39 +0100

    adding comments to Ex15.rb

--- Ex15.rb
+++ Ex15.rb
@@ -1,13 +1,18 @@
+# filename is the 'argument' being passed
 filename = ARGV.first
 
+# opening the file and assigning it's contents to a variable
 txt = open(filename)
 
+# calling the .read command on our variable with no parameters
 puts "Here's your file #{filename}:"
 print txt.read
 
+# adding the filename to another variable using gets.chomp
 print "\nType the filename again: "
 file_again = $stdin.gets.chomp
 
+# same process as before
 txt_again = open(file_again)
 
 print txt_again.read
