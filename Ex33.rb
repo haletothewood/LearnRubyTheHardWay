99470764b26abd257deef3876b34e95a6dc80533
David Halewood <davidnhalewood@hotmail.com>
2017-06-26 12:51:47 +0100

    fixing function and adding examples

--- Ex33.rb
+++ Ex33.rb
@@ -1,17 +1,18 @@
 def loop(i, numbers)
 	while i < 6
-		puts "At the top i is #{i}"
+		puts "\nAt the top i is #{i}"
 		numbers << i
 
 		i += 1
 		puts "Numbers now: ", numbers
 		puts "At the bottom i is #{i}"
+		puts "The numbers: "
+		numbers.each do |i| print "#{i} "; end
+		#numbers.each {|i| puts i}
 	end
 end
 
-loop(0, [])
+loop(-4, [-6, -5])
 
-puts "The numbers: "
+loop(3, ["This", "is", 'cool'])
 
-numbers.each do |i| print "#{i} "; end
-#numbers.each {|i| puts i}
