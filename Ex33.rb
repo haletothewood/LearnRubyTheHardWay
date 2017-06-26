e77ab53b13ee20d95272cbf53eb56d9a77870f45
David Halewood <davidnhalewood@hotmail.com>
2017-06-26 12:41:07 +0100

    refactoring into a function

--- Ex33.rb
+++ Ex33.rb
@@ -1,14 +1,15 @@
-i = 0
-numbers = []
+def loop(i, numbers)
+	while i < 6
+		puts "At the top i is #{i}"
+		numbers << i
 
-while i < 6
-	puts "At the top i is #{i}"
-	numbers << i
-
-	i += 1
-	puts "Numbers now: ", numbers
-	puts "At the bottom i is #{i}"
+		i += 1
+		puts "Numbers now: ", numbers
+		puts "At the bottom i is #{i}"
+	end
 end
+
+loop(0, [])
 
 puts "The numbers: "
 
